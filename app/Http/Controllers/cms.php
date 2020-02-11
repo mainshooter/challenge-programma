2d6b1cf1de537500828297437a6247e0e96ebb8a
Peter Romijn <peter@samebestdevelopment.nl>
2020-02-11 10:20:50 +0100

    Added create page

--- app/Http/Controllers/cms.php
+++ app/Http/Controllers/cms.php
@@ -7,6 +7,10 @@ use App\Page;
 
 class Cms extends Controller
 {
+    /**
+     * Presents a table with all pages
+     * @param  Request $request
+     */
     public function index(Request $request) {
       $Pages = Page::all();
       return view("cms/index", [
@@ -14,6 +18,26 @@ class Cms extends Controller
       ]);
     }
 
+    /**
+     * Get method to view create page
+     * @param  Request $request
+     */
+    public function createPage(Request $request) {
+      return view("cms/create");
+    }
+
+    /**
+     * Post method to handle post request to create page
+     * @param  Request $request
+     */
+    public function create(Request $request) {
+
+    }
+
+    /**
+     * All urls are redirected trough here to view the page
+     * @param  Request $request
+     */
     public function viewPage(Request $request) {
 
     }
