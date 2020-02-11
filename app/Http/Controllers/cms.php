680053a2bd0ebdcc585021faacea4544fc6ced5b
Peter Romijn <peter@samebestdevelopment.nl>
2020-02-11 10:06:41 +0100

    Added index view

--- app/Http/Controllers/cms.php
+++ app/Http/Controllers/cms.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Page;
 
-class cms extends Controller
+class Cms extends Controller
 {
     public function index(Request $request) {
-
+      $Pages = Page::all();
+      return view("cms/index", [
+        "Pages" => $Pages,
+      ]);
     }
 
     public function viewPage(Request $request) {
