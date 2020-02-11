5cd79b23654abbe9be64383cd382442105f781bd
Peter Romijn <peter@samebestdevelopment.nl>
2020-02-11 09:54:33 +0100

    Added model and controller

---
+++ app/Http/Controllers/Cms.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class cms extends Controller
+{
+    public function index(Request $request) {
+
+    }
+
+    public function viewPage(Request $request) {
+
+    }
+}
