9c9af5d573426192b91f5b5e9c2dcc85c1e939f7
IkBender <49744797+IkBender@users.noreply.github.com>
2020-02-16 20:19:07 +0100

    homepage review layout

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,15 +2,17 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/carousel.css') }}" >
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/homepage.css') }}" >
 
     <title>Startpagina</title>
 </head>
 <body>
-    <h1> Startpagina </h1>
+    <div class="div">
+       <h1> Startpagina </h1>
+    </div>
     <div id="carousel-homepage" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carousel-homepage" data-slide-to="0" class="active"></li>
@@ -43,6 +45,64 @@
             <span class="sr-only">Next</span>
         </a>
     </div>
+
+    <div class="cards">
+        <H3>Wat mensen over ons zeggen: </H3>
+        <div class="row">
+            <div class="col-sm-auto">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Bedrijf naam</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Naam van reviewer</h6>
+                        <p class="card-text">Voorbeeld van wat voor een inhoud een review kan hebben rewwre ww  wewr ewerw erwer .</p>
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-auto">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Bedrijf naam</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Naam van reviewer</h6>
+                        <p class="card-text">Voorbeeld van wat voor een inhoud een review kan hebben rewwre ww  wewr ewerw erwer .</p>
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-auto">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Bedrijf naam</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Naam van reviewer</h6>
+                        <p class="card-text">Voorbeeld van wat voor een inhoud een review kan hebben rewwre ww  wewr ewerw erwer.</p>
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-auto">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Bedrijf naam</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Naam van reviewer</h6>
+                        <p class="card-text">Voorbeeld van wat voor een inhoud een review kan hebben rewwre ww  wewr ewerw erwer .</p>
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+
+
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
