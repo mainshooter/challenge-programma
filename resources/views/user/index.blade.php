8061aa821abd93b1e6b21206deb91be48be79655
Peter Romijn <peter@samebestdevelopment.nl>
2020-02-17 21:51:26 +0100

    Display users

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,27 @@
+<div class="row">
+  <div class="col-12">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>E-mail</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($aUsers as $User)
+          <tr>
+            <td>{{$User->id}}</td>
+            <td>{{$User->name}}</td>
+            <td>{{$User->email}}</td>
+            <td>
+              <a href="#">Bewerken</a>
+              <a href="#">Verwijderen</a>
+            </td>
+          </tr>
+        @endforeach
+      </tobdy>
+    </table>
+  </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix("admin")->group(function() {
+  Route::get("user", "UserController@index")->name("user.index");
+});
