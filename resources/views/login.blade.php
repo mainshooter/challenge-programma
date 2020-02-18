4202bb6f57d768cedc4d399268719e968eff26a5
Onyi <onyilam@hotmail.com>
2020-02-18 11:39:47 +0100

    wip

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -27,7 +27,7 @@
 </form>
 </div> --}}
 
-<div class="row">
+<div class="row, mx-auto" >
     <div class="col">
         <h1>Inloggen</h1>
         </div>
@@ -38,7 +38,7 @@
             <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            {{-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> --}}
             </div>
             <div class="form-group">
             <label for="exampleInputPassword1">Password</label>
@@ -46,9 +46,11 @@
             </div>
             <div class="form-check">
             <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            <label class="form-check-label" for="exampleCheck1">Ingelogd blijven</label>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Inloggen</button>            
+            <button type="submit" class="btn btn-primary">Wachtwoord vergeten</button>
+
         </form>
     </div>
 </div>
