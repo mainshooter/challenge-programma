e4f58995510d96fe900893f530bfd08ac25b293e
IkBender <49744797+IkBender@users.noreply.github.com>
2020-02-18 10:20:46 +0100

    Responsive fix

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -10,10 +10,10 @@
     <title>Startpagina</title>
 </head>
 <body>
-    <div class="div">
-       <h1> Startpagina </h1>
-    </div>
-    <div id="carousel-homepage" class="carousel slide" data-ride="carousel">
+    <div class="row">
+        <div class="col-md-3">
+        </div>
+        <div id="carousel-homepage" class="carousel slide col-md-6" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carousel-homepage" data-slide-to="0" class="active"></li>
                 <li data-target="#carousel-homepage" data-slide-to="1"></li>
@@ -36,69 +36,62 @@
                 </div>
             </div>
 
-        <a class="carousel-control-prev" href="#carousel-homepage" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carousel-homepage" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+            <a class="carousel-control-prev" href="#carousel-homepage" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carousel-homepage" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+        <div class="col-md-3"></div>
     </div>
+
 
     <div class="cards">
         <H3>Wat mensen over ons zeggen: </H3>
         <div class="row">
-            <div class="col-sm-3">
-                <div class="card" style="width: 18rem;">
+            <div class="col-md-2"></div>
+            <div class="col-md-2">
+                <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Bedrijf naam</h5>
                         <h6 class="card-subtitle mb-2 text-muted">Naam van reviewer</h6>
                         <p class="card-text">Voorbeeld van wat voor een inhoud een review kan hebben rewwre ww  wewr ewerw erwer .</p>
                     </div>
-                    <div class="card-body">
-
-                    </div>
                 </div>
             </div>
-            <div class="col-sm-3">
-                <div class="card" style="width: 18rem;">
+            <div class="col-md-2">
+                <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Bedrijf naam</h5>
                         <h6 class="card-subtitle mb-2 text-muted">Naam van reviewer</h6>
                         <p class="card-text">Voorbeeld van wat voor een inhoud een review kan hebben rewwre ww  wewr ewerw erwer .</p>
                     </div>
-                    <div class="card-body">
-
-                    </div>
                 </div>
             </div>
 
-            <div class="col-sm-3">
-                <div class="card" style="width: 18rem;">
+            <div class="col-md-2">
+                <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Bedrijf naam</h5>
                         <h6 class="card-subtitle mb-2 text-muted">Naam van reviewer</h6>
                         <p class="card-text">Voorbeeld van wat voor een inhoud een review kan hebben rewwre ww  wewr ewerw erwer.</p>
                     </div>
-                    <div class="card-body">
-
-                    </div>
                 </div>
             </div>
 
-            <div class="col-sm-3">
-                <div class="card" style="width: 18rem;">
+            <div class="col-md-2">
+                <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Bedrijf naam</h5>
                         <h6 class="card-subtitle mb-2 text-muted">Naam van reviewer</h6>
                         <p class="card-text">Voorbeeld van wat voor een inhoud een review kan hebben rewwre ww  wewr ewerw erwer .</p>
                     </div>
-                    <div class="card-body">
-
-                    </div>
                 </div>
             </div>
+            <div class="col-md-2"></div>
         </div>
         </div>
 
