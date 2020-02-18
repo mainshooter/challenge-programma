3fc153cb5c93b052ad0c3499b08314a373fb5bc3
Peter Romijn <peter@samebestdevelopment.nl>
2020-02-18 17:43:23 +0100

    Layout

--- resources/views/cms/edit.blade.php
+++ resources/views/cms/edit.blade.php
@@ -1,42 +1,50 @@
-<form class="form" method="post">
-  @csrf
-  <div class="form-group">
-    <label>Title</label>
-    <input type="text" name="page_title" value="{{ $oPage->title }}" class="form-validation" required>
-  </div>
+@extends('layout')
 
-  <div class="form-group">
-    <label>url slug</label>
-    <input type="text" name="url_slug" value="{{ $oPage->slug }}" class="form-validation" required>
-  </div>
-
-  <div class="form-group">
-    <label>Content</label>
-    <div id="editor">
-      {!! $oPage->content !!}
+@section('content')
+  <a href='{{ route("cms.index") }}'>
+    <button type="button" class="btn btn-primary">Terug</button>
+  </a>
+  <br>
+  <form class="form" method="post">
+    @csrf
+    <div class="form-group">
+      <label>Title</label>
+      <input type="text" name="page_title" value="{{ $oPage->title }}" class="form-validation form-control" required>
     </div>
-  </div>
 
-  <input type="hidden" name="page_content">
+    <div class="form-group">
+      <label>url slug</label>
+      <input type="text" name="url_slug" value="{{ $oPage->slug }}" class="form-validation form-control" required>
+    </div>
 
-  <input type="submit" name="" class="btn btn-primary">
-</form>
+    <div class="form-group">
+      <label>Content</label>
+      <div id="editor">
+        {!! $oPage->content !!}
+      </div>
+    </div>
 
-<!-- Main Quill library -->
-<script src="//cdn.quilljs.com/1.3.6/quill.js"></script>
-<script src="//cdn.quilljs.com/1.3.6/quill.min.js"></script>
+    <input type="hidden" name="page_content">
 
-<!-- Theme included stylesheets -->
-<link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
-<link href="//cdn.quilljs.com/1.3.6/quill.bubble.css" rel="stylesheet">
+    <input type="submit" name="" class="btn btn-primary">
+  </form>
 
-<script>
-  let quill = new Quill('#editor', {
-    theme: 'snow'
-  });
-  let form = document.querySelector("form");
-  form.addEventListener('submit', () => {
-    let pageContentInput = document.querySelector("input[name=page_content]");
-    pageContentInput.value = document.querySelector(".ql-editor").innerHTML;
-  });
-</script>
+  <!-- Main Quill library -->
+  <script src="//cdn.quilljs.com/1.3.6/quill.js"></script>
+  <script src="//cdn.quilljs.com/1.3.6/quill.min.js"></script>
+
+  <!-- Theme included stylesheets -->
+  <link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+  <link href="//cdn.quilljs.com/1.3.6/quill.bubble.css" rel="stylesheet">
+
+  <script>
+    let quill = new Quill('#editor', {
+      theme: 'snow'
+    });
+    let form = document.querySelector("form");
+    form.addEventListener('submit', () => {
+      let pageContentInput = document.querySelector("input[name=page_content]");
+      pageContentInput.value = document.querySelector(".ql-editor").innerHTML;
+    });
+  </script>
+@endsection
--- resources/views/cms/index.blade.php
+++ resources/views/cms/index.blade.php
@@ -1,6 +1,10 @@
+@extends('layout')
+
+@section('content')
 <a href="{{@route('cms.create')}}">
   <button type="button" class="btn btn-primary">Pagina toevoegen</button>
 </a>
+<br>
 <table class="table">
   <thead>
     <tr>
@@ -15,10 +19,11 @@
         <td>{{$Page->title}}</td>
         <td>{{$Page->last_update}}</td>
         <td>
-          <a href="{{ route('cms.edit', $Page->page_id) }}">Edit page</a>
-          <a href="{{ route('cms.delete', $Page->page_id) }}">Delete page</a>
+          <a href="{{ route('cms.edit', $Page->page_id) }}" class="btn btn-secondary">Edit</a>
+          <a href="{{ route('cms.delete', $Page->page_id) }}" class="btn btn-danger">Delete</a>
         </td>
       </tr>
     @endforeach
   </tbody>
 </table>
+@endsection
