1fdc4cd921db7fcc028151a983213bdc24f1bde3
Peter Romijn <peter@samebestdevelopment.nl>
2020-02-19 14:22:08 +0100

    Dry js

--- resources/views/cms/create.blade.php
+++ resources/views/cms/create.blade.php
@@ -28,23 +28,16 @@
 
     <input type="submit" name="" value="Opslaan" class="btn btn-primary">
   </form>
+@endsection
+@section('head')
 
   <!-- Main Quill library -->
-  <script src="//cdn.quilljs.com/1.3.6/quill.js"></script>
-  <script src="//cdn.quilljs.com/1.3.6/quill.min.js"></script>
+  <script src="//cdn.quilljs.com/1.3.6/quill.js" defer></script>
+  <script src="//cdn.quilljs.com/1.3.6/quill.min.js" defer></script>
 
   <!-- Theme included stylesheets -->
   <link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
   <link href="//cdn.quilljs.com/1.3.6/quill.bubble.css" rel="stylesheet">
 
-  <script>
-    let quill = new Quill('#editor', {
-      theme: 'snow'
-    });
-    let form = document.querySelector("form");
-    form.addEventListener('submit', () => {
-      let pageContentInput = document.querySelector("input[name=page_content]");
-      pageContentInput.value = document.querySelector(".ql-editor").innerHTML;
-    });
-  </script>
+  <script src="{{ asset('js/cmsEditor.js') }}" defer></script>
 @endsection
--- resources/views/cms/edit.blade.php
+++ resources/views/cms/edit.blade.php
@@ -28,25 +28,18 @@
 
     <input type="hidden" name="page_content">
 
-    <input type="submit" name="" class="btn btn-primary">
+    <input type="submit" value="Opslaan" class="btn btn-primary">
   </form>
+@endsection
+@section('head')
 
   <!-- Main Quill library -->
-  <script src="//cdn.quilljs.com/1.3.6/quill.js"></script>
-  <script src="//cdn.quilljs.com/1.3.6/quill.min.js"></script>
+  <script src="//cdn.quilljs.com/1.3.6/quill.js" defer></script>
+  <script src="//cdn.quilljs.com/1.3.6/quill.min.js" defer></script>
 
   <!-- Theme included stylesheets -->
   <link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
   <link href="//cdn.quilljs.com/1.3.6/quill.bubble.css" rel="stylesheet">
 
-  <script>
-    let quill = new Quill('#editor', {
-      theme: 'snow'
-    });
-    let form = document.querySelector("form");
-    form.addEventListener('submit', () => {
-      let pageContentInput = document.querySelector("input[name=page_content]");
-      pageContentInput.value = document.querySelector(".ql-editor").innerHTML;
-    });
-  </script>
+  <script src="{{ asset('js/cmsEditor.js') }}" defer></script>
 @endsection
