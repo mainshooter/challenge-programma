54f136e724599c62db37af1cf1931be0c6db328f
IkBender <49744797+IkBender@users.noreply.github.com>
2020-03-01 14:49:32 +0100

    middleware setup

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/homepage', 'HomepageController@index');
 
 Route::get("page/{any}", "Cms@viewPage")->name('cms.view')->where("any", ".*");
 
-Route::get('home', 'HomeController@index')->name('home');
+Route::get('home', 'HomePageController@index')->name('home');
 // Route::get('/', 'Auth\RegisterController@register_global')->name('register_global');
 // Route::get('/', 'Auth\RegisterController@register_students')->name('register_students');
 
@@ -34,18 +34,19 @@ Route::get('register_company', 'Auth\RegisterController@showCompanyRegisterForm'
 Auth::routes();
 
 
+Route::middleware('role:admin')->group(function () {
+    Route::prefix("admin")->group(function() {
+      Route::prefix("user")->group(function() {
+        Route::get("/", "UserController@index")->name("user.index");
+      });
+      Route::prefix('cms')->group(function() {
+        Route::get('/', "Cms@index")->name("cms.index");
+        Route::get('create', "Cms@createPage")->name("cms.create");
+        Route::get('edit/{id}', "Cms@editPage")->name("cms.edit");
+        Route::get('delete/{id}', "Cms@delete")->name('cms.delete');
 
-Route::prefix("admin")->group(function() {
-  Route::prefix("user")->group(function() {
-    Route::get("/", "UserController@index")->name("user.index");
-  });
-  Route::prefix('cms')->group(function() {
-    Route::get('/', "Cms@index")->name("cms.index");
-    Route::get('create', "Cms@createPage")->name("cms.create");
-    Route::get('edit/{id}', "Cms@editPage")->name("cms.edit");
-    Route::get('delete/{id}', "Cms@delete")->name('cms.delete');
-
-    Route::post("edit/{id}", 'Cms@edit')->name('cms.edit.post');
-    Route::post('create', "Cms@create")->name("cms.create.post");
-  });
+        Route::post("edit/{id}", 'Cms@edit')->name('cms.edit.post');
+        Route::post('create', "Cms@create")->name("cms.create.post");
+      });
+    });
 });
