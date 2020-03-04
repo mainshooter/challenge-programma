390797b8bab93fa30b319c69002d1b18e5414fc6
Peter Romijn <peter@samebestdevelopment.nl>
2020-03-04 16:40:15 +0100

    Wip

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,17 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\User;
 
 class UserController extends Controller
 {
     public function index(Request $request) {
-      $aUsers = \App\User::all();
+      $aUsers = User::all();
       return view("user/index", [
         'aUsers' => $aUsers
       ]);
     }
 
     public function edit(Request $request, $iId) {
+      $validatedData = $request->validate([
+        "name" => "required",
+      ]);
+
+      $oUser = User::find($iId);
+      if (is_null($oUser)) {
+        return redirect()->route('user.index');
+      }
+
+      $oUser->name = $request->name;
+
 
     }
 }
