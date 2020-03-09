0e12bf40b04a9dd8b7a63168b9ac194138b14ef3
IkBender <49744797+IkBender@users.noreply.github.com>
2020-03-09 21:40:08 +0100

    tests

--- resources/views/NewStudent/index.blade.php
+++ resources/views/NewStudent/index.blade.php
@@ -22,8 +22,8 @@
                             <td>{{$Student->email}}</td>
                             <td>{{$Student->schoolyear}}</td>
                             <td>
-                                <a href="{{ route('newstudent.accept', $Student->id) }}" class="btn btn-success">Accepteren</a>
-                                <a href="{{ route('newstudent.delete', $Student->id) }}" class="btn btn-danger">Verwijderen</a>
+                                <a href="{{ route('newstudent.accept', $Student->id) }}" id="accept" class="btn btn-success">Accepteren</a>
+                                <a href="{{ route('newstudent.delete', $Student->id) }}" id="delete" class="btn btn-danger">Verwijderen</a>
                             </td>
                         </tr>
                     @endforeach
--- tests/Browser/StudentAcceptTest.php
+++ tests/Browser/StudentAcceptTest.php
@@ -5,6 +5,7 @@ namespace Tests\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
+use App\User;
 
 class StudentAcceptTest extends DuskTestCase
 {
@@ -15,9 +16,19 @@ class StudentAcceptTest extends DuskTestCase
      */
     public function testAccept() {
         $this->browse(function(Browser $browser) {
-            $browser->visit('admin/newStudent')
-/*                    ->click("a.btn.btn-success")*/
-                    ->assertPathIs("/admin/newStudent");
+            $browser->loginAs(User::where('email', 'admin@gmail.com')->first())
+                    ->visit('admin/newStudent')
+                    ->click("a[id=accept]")
+                    ->assertPathIs("/admin/newStudent/accept/1");
+        });
+    }
+
+    public function testDecline() {
+        $this->browse(function(Browser $browser) {
+            $browser->loginAs(User::where('email', 'admin@gmail.com')->first())
+                ->visit('admin/newStudent')
+                ->click("a[id=delete]")
+                ->assertPathIs("/admin/newStudent/delete/1");
         });
     }
 }
