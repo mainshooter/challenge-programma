7a801c9dfad691bd1e0f7d532dc4ceb3233a6aba
Peter Romijn <paromijn@avans.nl>
2020-03-10 14:39:51 +0100

    Test register (#26)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,7 +24,7 @@
                                         <a class="nav-link" data-toggle="tab" href="#company" role="tab" aria-controls="Two" aria-selected="false">Bedrijven</a>
                                     </li>
                                 </ul>
-                                {{-- <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" 
+                                {{-- <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
                                     name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
@@ -40,7 +40,7 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Voornaam') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" 
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
                                     name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
@@ -56,7 +56,7 @@
                             <label for="infixname" class="col-md-4 col-form-label text-md-right">{{ __('Tussenvoegsel') }}</label>
 
                             <div class="col-md-6">
-                                <input id="infixname" type="text" class="form-control @error('infixname') is-invalid @enderror" 
+                                <input id="infixname" type="text" class="form-control @error('infixname') is-invalid @enderror"
                                     name="infixname" value="{{ old('infixname') }}" required autocomplete="infixname" autofocus>
 
                                 @error('infixname')
@@ -73,7 +73,7 @@
                             <label for="lastname" class="col-md-4 col-form-label text-md-right">{{ __('Achternaam') }}</label>
 
                             <div class="col-md-6">
-                                <input id="lastname" type="text" class="form-control @error('lastname') is-invalid @enderror" 
+                                <input id="lastname" type="text" class="form-control @error('lastname') is-invalid @enderror"
                                     name="lastname" value="{{ old('lastname') }}" required autocomplete="lastname" autofocus>
 
                                 @error('lastname')
@@ -83,15 +83,15 @@
                                 @enderror
                             </div>
                         </div>
-                        
+
                         {{-- leerjaar --}}
                         <div class="form-group row">
                             <label for="schoolyear" class="col-md-4 col-form-label text-md-right">{{ __('Studiejaar') }}</label>
 
                             <div class="col-md-2">
-                                <input id="schoolyear" type="number" class="form-control @error('schoolyear') is-invalid @enderror" 
+                                <input id="schoolyear" type="number" class="form-control @error('schoolyear') is-invalid @enderror"
                                     name="schoolyear" value="1" min="1" max="4" step="1" value="{{ old('schoolyear') }}" required >
-                        
+
                                 @error('schoolyear')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -132,7 +132,7 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Wachtwoord herhalen') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control" name="password-confirm" required autocomplete="new-password">
                             </div>
                         </div>
 
