7203dedb7a95b4807b4962ac2aed241cc8991110
Peter Romijn <peter@samebestdevelopment.nl>
2020-03-13 10:16:18 +0100

    Added school year back

--- resources/views/user/accept.blade.php
+++ resources/views/user/accept.blade.php
@@ -25,7 +25,7 @@
                             <td>{{$oUser->prefix}}</td>
                             <td>{{$oUser->lastname}}</td>
                             <td>{{$oUser->email}}</td>
-                            <td>{{$oUser->schoolyear}}</td>
+                            <td>{{$oUser->studentInfo->school_year }}</td>
                             <td>
                                 <a href="{{ route('user.accept', $oUser->id) }}" id="accept" class="btn btn-success">Accepteren</a>
                                 <a href="{{ route('user.delete', $oUser->id) }}" id="delete" class="btn btn-danger">Verwijderen</a>
