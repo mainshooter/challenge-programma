23f0747db2075bde0f1feb68d943b7b7600bd078
Peter Romijn <peter@samebestdevelopment.nl>
2020-03-13 11:40:09 +0100

    Fixed mangement menu

--- resources/views/management/index.blade.php
+++ resources/views/management/index.blade.php
@@ -11,7 +11,7 @@
                 <div class="menuDiv" >
                     <nav class="nav flex-column">
                         <a class="nav-link" href="{{ route('user.index') }}">Gebruikersbeheer</a>
-                        <a class="nav-link" href="{{ route('user.not.accepted.overview') }}">Accepteren studenten</a>
+                        <a class="nav-link" href="{{ route('user.not.accepted.overview') }}">Accepteren gebruikers</a>
                         <a class="nav-link" href="{{ route('event.index') }}">Aanmaken evenement</a>
                         <a class="nav-link" href="{{ route('cms.index') }}">CMS</a>
                         <a class="nav-link" href="{{ route('image.index')  }}">Carousel</a>
