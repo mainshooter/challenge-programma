e053629e78bd030399e2ad219413db61c3050a4a
Versteeg0 <49746367+Versteeg0@users.noreply.github.com>
2020-03-21 12:45:25 +0100

    First setup loading events.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,18 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Event;
 use Auth;
 use Illuminate\Http\Request;
+use MongoDB\Driver\Session;
 
 class ProfileController extends Controller
 {
     public function index(Request $request) {
-        return view('profile/index');
+        $oUser = Auth::user();
+        $aEvents = Event::all()->where(students.contains($oUser));
+
+        return view('profile/index', ["aEvents" => $aEvents]);
     }
 
     public function terminatePage(Request $request) {
-        $oUser = User::find(Auth::user()->id);
-        return view("profile/terminate", ["oUser" => $oUser]);
+        return view("profile/terminate");
     }
 
     public function terminate(Request $request) {
@@ -23,7 +27,8 @@ class ProfileController extends Controller
         Auth::logout();
 
         if($oUser->delete()) {
-            return redirect()->route('profile.index')->with('global', 'Je ben uitgeschreven.');
+            Session::flash('message', 'Je bent uitgeschreven');
+            return redirect()->route('profile.index');
         }
     }
 }
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -15,6 +15,15 @@
                         <th>datum</th>
                     </tr>
                     </thead>
+                    <tbody>
+                    @foreach($aEvents as $oEvent)
+                        <td>{{$oEvent->id}}</td>
+                        <td>{{$oEvent->name}}</td>
+                        <td>{{$oEvent->event_start_date_time}}</td>
+                        <td>{{$oEvent->street}} {{$oEvent->house_number}}, {{$oEvent->city}}</td>
+                        <td>{{$oEvent->zipcode}}</td>
+                        <td>{{$oEvent->points}}</td>
+                    </tbody>
                 </table>
                 <td><a href="{{ route('profile.terminate')}}" class="btn btn-danger">Uitschrijven</a>
             </div>
