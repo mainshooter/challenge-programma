d7abc39d0f2b06cd70f2dd3680a1e8f7d9da8f9e
Peter Romijn <peter@samebestdevelopment.nl>
2020-03-22 21:05:30 +0100

    Test

--- tests/Browser/ReviewTest.php
+++ tests/Browser/ReviewTest.php
@@ -22,13 +22,14 @@ class ReviewTest extends DuskTestCase
         });
     }
 
+    /** @test */
     public function CreateReviewTest() {
       $this->browse(function(Browser $browser) {
         $browser->loginAs(User::where('email', 'avans@gmail.com')->first());
         $browser->visit('/reviews');
         $browser->press('Review toevoegen');
         $browser->type('review_stars', 8);
-        $browser->value('.ql-editor', 'Het evenement dat is georganiseerd was echt leuk! We hebben ook nieuwe dingen geleerd van de studenten');
+        $browser->type('.ql-editor', 'Het evenement dat is georganiseerd was echt leuk! We hebben ook nieuwe dingen geleerd van de studenten');
         $browser->press('Toevoegen');
         $browser->assertSee('Het evenement dat is georganiseerd was echt leuk! We hebben ook nieuwe dingen geleerd van de studenten');
       });
