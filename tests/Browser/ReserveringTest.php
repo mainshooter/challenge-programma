2299bbc4894dface9221c1e4ef17e480b308dbc7
IkBender <49744797+IkBender@users.noreply.github.com>
2020-03-25 10:07:13 +0100

    Evenement reservering tests

---
+++ tests/Browser/ReserveringTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ReserveringTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testEventManagementPage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::where('email', 'admin@gmail.com')->First());
+            $browser->visit('/admin/event')
+                    ->assertSee('Goedgekeurde evenementen');
+        });
+    }
+
+    public function testEventDetailsPage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::where('email', 'admin@gmail.com')->First());
+            $browser->visit('/admin/event')
+                ->Clicklink('Details')
+                ->assertSee('Evenement details');
+        });
+    }
+}
