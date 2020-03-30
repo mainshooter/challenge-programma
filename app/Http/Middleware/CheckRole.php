bd76326d2181159f893fbcaec19f9f4716515b67
Peter Romijn <paromijn@avans.nl>
2020-03-30 11:51:07 +0200

    Fix agenda verzoek (#70)

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -21,10 +21,13 @@ class CheckRole
           return redirect('login');
         }
 
-        if(auth()->user()->role == $role){
-            return $next($request);
-        }
+        $aRoles = explode("|", $role);
 
+        foreach ($aRoles as $sRole) {
+          if(auth()->user()->role == $sRole){
+              return $next($request);
+          }
+        }
 
         return redirect('home');
     }
