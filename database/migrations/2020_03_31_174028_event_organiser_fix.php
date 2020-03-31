56441b300f94a6fe578a43323f5edd3c21bea469
Peter Romijn <paromijn@avans.nl>
2020-03-31 20:29:32 +0200

    Fix (#72)
    * Fix
    
    * Fix foreign
    
    Co-authored-by: Peter Romijn <peter@samebestdevelopment.nl>

--- database/migrations/2020_03_31_174028_event_organiser_fix.php
+++ database/migrations/2020_03_31_174028_event_organiser_fix.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class EventOrganiser extends Migration
+class EventOrganiserFix extends Migration
 {
     /**
      * Run the migrations.
@@ -15,6 +15,7 @@ class EventOrganiser extends Migration
     {
         Schema::table('event', function(Blueprint $table) {
             $table->unsignedBigInteger('user_id')->nullable()->change();
+            $table->dropForeign('event_user_id_foreign');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
         });
     }
