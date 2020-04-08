94f20fa1292edefce16c6b76b47cf14726e3ae1b
Onyi <onyilam@hotmail.com>
2020-04-08 21:40:15 +0200

    wip

--- resources/views/photoalbum/index.blade.php
+++ resources/views/photoalbum/index.blade.php
@@ -1,30 +1,81 @@
 @extends('layout')
-
+@section('head')
+<link rel="stylesheet" type="text/css" href="{{ asset('css/galery.css') }}">
+@endsection
 @section('content')
-    <div class="container no-max-width">
-        <div class="row">
-            <div class="col-4">
-            </div>
 
-            <div class="col-4">
-                <h1>Timeline ChallengeProgramma</h1>
-                <div class="card">
-                    <h2>Evenementnaam</h2>
-                    <h5>Bedrijfsnaam, 07-04-2020</h5>
-                    <div class="fakeimg" style="height:100px;">Imagegroup</div>
-                    <p>Kleine beschrijving toegevoegd door Admin</p>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8 p-4">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Evenementnaam</h4>
                 </div>
-                <br>
-                <div class="card">
-                    <h2>Evenementnaam</h2>
-                    <h5>Bedrijfsnaam, 07-04-2020</h5>
-                    <div class="fakeimg" style="height:100px;">Imagegroup</div>
-                    <p>Kleine beschrijving toegevoegd door Admin</p>
+                <div class="card-body">
+                    <h5 class="p-2">Bedrijfsnaam, 07-04-2020</h5>
+                    <div class="widget-container">
+                        <div class="widget row image-tile">
+                            <div class="tile col-md-5"
+                                style="background: url('https://picsum.photos/300') no-repeat center top; background-size: cover;">
+                                <p>Kleine beschrijving toegevoegd door Admin</p>
+                            </div>
+                            <div class="tile col-md-5"
+                                style="background: url('https://picsum.photos/400') no-repeat center top; background-size: cover;">
+                                <p>Kleine beschrijving toegevoegd door Admin</p>
+                            </div>
+                            <div class="tile col-md-5"
+                                style="background: url('https://picsum.photos/500') no-repeat center top; background-size: cover;">
+                                <p>Kleine beschrijving toegevoegd door Admin</p>
+                            </div>
+                            <div class="tile col-md-5"
+                                style="background: url('https://picsum.photos/600') no-repeat center top; background-size: cover;">
+                                <p>Kleine beschrijving toegevoegd door Admin</p>
+                            </div>
+                            <div class="tile more-images col-md-2">
+                                <div class="images-number">42+</div>
+                                Foto's
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+        </div>
 
-            <div class="col-4">
+        <div class="col-md-8 p-4">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Evenementnaam</h4>
+                </div>
+                <div class="card-body">
+                    <h5 class="p-2">Bedrijfsnaam, 07-04-2020</h5>
+                    <div class="widget-container">
+                        <div class="widget row image-tile">
+                            <div class="tile col-md-5"
+                                style="background: url('https://picsum.photos/300') no-repeat center top; background-size: cover;">
+                                <p>Kleine beschrijving toegevoegd door Admin</p>
+                            </div>
+                            <div class="tile col-md-5"
+                                style="background: url('https://picsum.photos/400') no-repeat center top; background-size: cover;">
+                                <p>Kleine beschrijving toegevoegd door Admin</p>
+                            </div>
+                            <div class="tile col-md-5"
+                                style="background: url('https://picsum.photos/500') no-repeat center top; background-size: cover;">
+                                <p>Kleine beschrijving toegevoegd door Admin</p>
+                            </div>
+                            <div class="tile col-md-5"
+                                style="background: url('https://picsum.photos/600') no-repeat center top; background-size: cover;">
+                                <p>Kleine beschrijving toegevoegd door Admin</p>
+                            </div>
+                            <div class="tile more-images col-md-2">
+                                <div class="images-number">42+</div>
+                                Foto's
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+</div>
+
 @endsection
