f5d747db462262ac4f7294d31acf6b5ac8147907
IkBender <49744797+IkBender@users.noreply.github.com>
2020-04-11 12:20:56 +0200

    cookie test (#103)
    * cookie test
    
    * cookie fix on every test
    
    * seeder fix
    
    * Fixed tests
    
    * accept user test fix
    
    Co-authored-by: Peter Romijn <paromijn@avans.nl>
    Co-authored-by: Versteeg0 <49746367+Versteeg0@users.noreply.github.com>
    Co-authored-by: Peter Romijn <peter@samebestdevelopment.nl>

---
+++ tests/Browser/CookieTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class CookieTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->clickLink('Akkoord')
+                    ->assertDontSee('Deze website gebruikt cookies');
+        });
+    }
+}
