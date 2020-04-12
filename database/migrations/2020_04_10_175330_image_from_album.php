f27067f14bdc1a03ab50c822ecfc8bd9e1033853
Onyi <onyilam@hotmail.com>
2020-04-12 10:35:37 +0200

    bigincrements

--- database/migrations/2020_04_10_175330_image_from_album.php
+++ database/migrations/2020_04_10_175330_image_from_album.php
@@ -14,7 +14,7 @@ class ImageFromAlbum extends Migration
     public function up()
     {
         Schema::create('image_from_album', function (Blueprint $table) {
-            $table->increments("id");
+            $table->bigIncrements("id");
             $table->string('path');
             $table->integer('photoalbum_id')->unsigned();
             $table->foreign('photoalbum_id')->references('id')->on('photoalbum')->onDelete('cascade');
