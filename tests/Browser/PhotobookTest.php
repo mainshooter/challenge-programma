5e96c125bc75e47b3124cd18e3f9f84e6b663b5a
liambonthond <61025700+liambonthond@users.noreply.github.com>
2020-04-20 19:55:47 +0200

    Test geschreven voor fotoboek aanmaken (#125)
    Co-authored-by: IkBender <49744797+IkBender@users.noreply.github.com>

--- tests/Browser/PhotobookTest.php
+++ tests/Browser/PhotobookTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser;
 
+use App\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
@@ -19,5 +20,20 @@ class PhotobookTest extends DuskTestCase
             $browser->visit('/photoalbum')
                     ->assertSee('Tijdlijn');
         });
+    }
+
+    public function testCreate()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::where('email', 'content@gmail.com')->First())
+                ->visit('/photoalbum')
+                ->clickLink("Akkoord")
+                ->clickLink('Nieuwe album toevoegen')
+                ->value("input[name=title]", "TestFotoalbum")
+                ->value("textarea[name=description]", "TestDescription")
+                ->click("input[type=submit]")
+                ->assertSee('Fotoalbum is aangemaakt');
+        } );
+
     }
 }
