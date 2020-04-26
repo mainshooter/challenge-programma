3e7b567fbbd5116266f8d92d063f9e3c124db647
Lars <Versteeg0@outlook.com>
2020-04-26 13:58:48 +0200

    Another RFC.

--- resources/views/photoalbum/index.blade.php
+++ resources/views/photoalbum/index.blade.php
@@ -14,6 +14,9 @@
         @endif
     </div>
         <div class="row justify-content-center">
+            @if($aPhotoalbum->isEmpty())
+                <h5>Er zijn op het moment geen fotoalbums beschikbaar</h5>
+            @endif
             @foreach ($aPhotoalbum as $album)
             <div class="col-md-8 p-4">
                 <div class="card">
