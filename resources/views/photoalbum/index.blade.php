07cffd43c10bb81431b26a8487b85d11301367f7
Lars <Versteeg0@outlook.com>
2020-04-26 14:01:36 +0200

    Wrong branch

--- resources/views/photoalbum/index.blade.php
+++ resources/views/photoalbum/index.blade.php
@@ -14,9 +14,6 @@
         @endif
     </div>
         <div class="row justify-content-center">
-            @if($aPhotoalbum->isEmpty())
-                <h5>Er zijn op het moment geen fotoalbums beschikbaar</h5>
-            @endif
             @foreach ($aPhotoalbum as $album)
             <div class="col-md-8 p-4">
                 <div class="card">
