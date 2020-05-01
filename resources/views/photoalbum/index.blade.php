f6b22c2d162731fbb45492e9ae7ab030538e572c
Peter Romijn <peter@samebestdevelopment.nl>
2020-05-01 10:21:25 +0200

    Hide empty photoalbums

--- resources/views/photoalbum/index.blade.php
+++ resources/views/photoalbum/index.blade.php
@@ -34,33 +34,36 @@
         @if($aPhotoalbum->isEmpty())
             <h5>Er zijn op het moment geen fotoalbums beschikbaar</h5>
         @endif
-        @foreach ($aPhotoalbum as $album)
+        @foreach ($aPhotoalbum as $oAlbum)
+        @if($oAlbum->photos->isEmpty())
+          @continue
+        @endif
         <div class="col-md-8 p-4">
             <div class="card">
                 <div class="card-header">
-                    <h5 class="m-0">{{$album->title}} - @if(!is_null($album->event)){{$album->event->name}}, {{$album->event->event_start_date_time}}@endif </h5>
+                    <h5 class="m-0">{{$oAlbum->title}} - @if(!is_null($oAlbum->event)){{$oAlbum->event->name}}, {{$oAlbum->event->event_start_date_time}}@endif </h5>
                 </div>
                 <div class="card-body">
                     @if(Auth::user() && (Auth::user()->role == 'admin' || Auth::user()->role == 'content-writer' ))
                     <a class="btn btn-primary float-right"
-                        href="{{route('photoalbum.edit', ['id' => $album->id])}}">Bewerken</a>
+                        href="{{route('photoalbum.edit', ['id' => $oAlbum->id])}}">Bewerken</a>
                     @endif
-                        <h5 class="p-2">{{$album->description}}</h5>
+                        <h5 class="p-2">{{$oAlbum->description}}</h5>
                         <div class="widget-container">
                             <div class="widget row image-tile">
-                                @foreach($album->photos as $oPhoto)
+                                @foreach($oAlbum->photos as $oPhoto)
                                     <div class="col-md-5">
                                         <img class="img-thumbnail rounded mx-auto d-block" src="{{Storage::url($oPhoto->path)}}">
                                     </div>
                                     @break($loop->index == 1)
                                 @endforeach
                                 <div class="tile more-images col-md-2">
-                                    <div class="images-number">{{count($album->photos)}}</div>
+                                    <div class="images-number">{{count($oAlbum->photos)}}</div>
                                     Foto's
                                 </div>
                             </div>
                             <a class="btn btn-primary float-left"
-                               href="{{route('photoalbum.photos', ['id' => $album->id])}}">Foto's bekijken</a>
+                               href="{{route('photoalbum.photos', ['id' => $oAlbum->id])}}">Foto's bekijken</a>
                         </div>
                 </div>
             </div>
