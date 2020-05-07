24ea85cc86729c2a306b3f91acf9f87177e978aa
Peter Romijn <peter@samebestdevelopment.nl>
2020-05-07 16:10:06 +0200

    Made overview

---
+++ resources/views/review/overview.blade.php
@@ -0,0 +1,35 @@
+@extends('layout')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1>Alle reviews</h1>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Bedrijf</th>
+              <th>Review</th>
+              <th>Sterren</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($aReviews as $oReview)
+              <tr>
+                <td>{{ $oReview->id }}</td>
+                <td>{{ $oReview->company->companyInfo->company_name }}</td>
+                <td>{!! $oReview->body !!}</td>
+                <td>{{ $oReview->rating }}</td>
+                <td>
+                  <a class="btn btn-danger" href="">Verwijderen</a>
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+@endsection
