eb3f0273fe57df7160b3e18c23d031180975fa86
Peter Romijn <paromijn@avans.nl>
2020-05-09 11:57:19 +0200

    Update image.blade.php

--- resources/views/image/image.blade.php
+++ resources/views/image/image.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="row">
         <div class="col-12">
-          <h1>Carousel</h1>
+          <h1>Carrousel</h1>
         </div>
         <div class="col-4">
           <div class="jumbotron">
