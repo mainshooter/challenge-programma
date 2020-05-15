46194f24503ec084262545b640e69ec7f1d5a6ae
Peter Romijn <paromijn@avans.nl>
2020-05-15 10:30:07 +0200

    Errors layout (#178)

---
+++ resources/views/errors/429.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="error-container">
+          <h1>Too Many Request</h1>
+          <h2>429</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
