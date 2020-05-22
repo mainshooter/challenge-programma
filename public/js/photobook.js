7007cbb28c3325714536e90d4176325348e34d7e
Peter Romijn <peter@samebestdevelopment.nl>
2020-05-22 23:21:41 +0200

    Fix

--- public/js/photobook.js
+++ public/js/photobook.js
@@ -1,8 +1,7 @@
-
 window.onload = function () {
     setSelectedIndex();
-    $('#selectBox').on('select2:select', function (e) {
-    e.target.form.submit();
+    element = document.getElementById('selectBox').addEventListener('change', (e) => {
+        e.target.form.submit();
     });
 };
 
@@ -11,12 +10,12 @@ function setSelectedIndex() {
     let selectBox = document.getElementById('selectBox');
 
     if (selectLabel.className == 'dateNew') {
-        $('selectBox').val(1);
+        selectBox.selectedIndex = 1;
     }
     else if (selectLabel.className == 'dateOld') {
-        $('selectBox').val(2);
+        selectBox.selectedIndex = 2;
     }
     else {
-        $('selectBox').val(0);
+        selectBox.selectedIndex = 0;
     }
 }
